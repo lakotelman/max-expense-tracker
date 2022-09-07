271e81230f7e063585bfd42d094eca8efcb5aa4f
lakotelman <la.n.kot@pm.me>
2022-09-07 13:12:54 -0800

    Form Component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import Expenses from "./components/Expenses/Expenses"
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Coffee',
+      id: "e1",
+      title: "Coffee",
       amount: 3.55,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
 
-
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses expenses={expenses}/>
+      
+      <NewExpense />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
